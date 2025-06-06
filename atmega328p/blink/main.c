b955f3ce33b84d3f12b202d4d5cb082483bb8809
Harvey <harveyn4444@gmail.com>
2025-06-06 10:01:51 +0100

    atmega328p blink

---
+++ atmega328p/blink/main.c
@@ -0,0 +1,21 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+
+	// Set PORTB5 as an output
+	DDRB = DDRB | (1<<DDB5);
+	while(1){
+	//set PORTB5
+	PORTB = PORTB | (1 << PORTB5);
+	
+	// wait
+	_delay_ms(500);
+
+	// Unset PORTB5
+	PORTB = PORTB & ~(1<<PORTB5);
+
+	// wait again
+	_delay_ms(500);
+	}
+}
--- attiny85/blink/main.c
+++ attiny85/blink/main.c
@@ -1,19 +1,20 @@
 /*
 https://github.com/mattwach/avr_pico/blob/main/examples/blink_attiny85/main.c
 */
+//#define F_CPU 16500000UL
 
 #include <stdint.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define PIN 3
+#define PIN 6
 
 int main(void) {
   DDRB = 1 << PIN;
   while (1) {
     PORTB = 1 << PIN;
-    _delay_ms(1000);
+    _delay_ms(2000);
     PORTB = ~(1 << PIN);
-    _delay_ms(100);
+    _delay_ms(1000);
   }
 }
